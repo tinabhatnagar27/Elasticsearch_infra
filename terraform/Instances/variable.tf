b5cbcca721f299e1c59c0d5718b38248fab77790
tinabhatnagar27 <154892884+tinabhatnagar27@users.noreply.github.com>
2025-01-17 19:43:34 +0530

    variable.tf

--- terraform/Instances/variable.tf
+++ terraform/Instances/variable.tf
@@ -18,21 +18,17 @@ variable "pri-sub-2-id" {
   type = string  
 }
 
-variable "pri-sub-3-id" {
-  type = string
-}
-
 variable "ami-id" {
   type = string
-  default = "ami-09b0a86a2c84101e1"
+  default = "ami-0672fd5b9210aa093"
 }
 
 variable "instance-type" {
   type = string
-  default = "t3.micro"
+  default = "t2.micro"
 }
 
 variable "key-name" {
   type = string
-  default = "ninja"
+  default = "role27"
 }
