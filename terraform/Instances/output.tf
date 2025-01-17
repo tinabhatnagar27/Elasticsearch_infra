1eba4d24f4c68d79c86efa2df53c1c2391d2c917
tinabhatnagar27 <154892884+tinabhatnagar27@users.noreply.github.com>
2025-01-17 19:39:01 +0530

    output.tf

--- terraform/Instances/output.tf
+++ terraform/Instances/output.tf
@@ -1,33 +1,25 @@
 #instenses ID
 output "pub-instance-id" {
-  value = aws_instance.redis-public.id
+  value = aws_instance.elasticsearch-public.id
 }
 
 output "private-instance1-id" {
-  value = aws_instance.redis-private-1.id
+  value = aws_instance.elasticsearch-private-1.id
 }
 
 output "private-instance2-id" {
-  value = aws_instance.redis-private-2
-}
-
-output "private-instance3-id" {
-  value = aws_instance.redis-private-3
+  value = aws_instance.elasticsearch-private-2
 }
 
 # instences IP addresses
 output "public-instance-ip" {
-  value = aws_instance.redis-public.public_ip
+  value = aws_instance.elasticsearch-public.public_ip
 }
 
 output "private-instance1-ip" {
-  value = aws_instance.redis-private-1.private_ip
+  value = aws_instance.elasticsearch-private-1.private_ip
 }
 
 output "private-instance2-ip" {
-  value = aws_instance.redis-private-2.private_ip
-}
-
-output "private-instance3-ip" {
-  value = aws_instance.redis-private-3.private_ip
+  value = aws_instance.elasticsearch-private-2.private_ip
 }
