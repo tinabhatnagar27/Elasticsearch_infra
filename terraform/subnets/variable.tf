92e30d28cc219373f457d3af4d28b207b084dd76
tinabhatnagar27 <154892884+tinabhatnagar27@users.noreply.github.com>
2025-01-17 20:07:00 +0530

    variable.tf

--- terraform/subnets/variable.tf
+++ terraform/subnets/variable.tf
@@ -16,14 +16,9 @@ variable "pri-sub2-az" {
   default = "ap-south-1b"
 }
 
-variable "pri-sub3-az" {
-  type = string
-  default = "ap-south-1c"
-}
-
 variable "pub-sub-az" {
   type = string
-  default = "ap-south-1b"
+  default = "ap-south-1a"
 }
 
 variable "pub-cidr" {
@@ -39,9 +34,4 @@ variable "pri-sub-1-cidr" {
 variable "pri-sub-2-cidr" {
   type = string
   default = "10.0.3.0/24"
-}
-
-variable "pri-sub-3-cidr" {
-  type = string
-  default = "10.0.4.0/24"
 }
