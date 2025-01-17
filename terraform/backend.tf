b4407b01514bc7a7ceca83ed78c1a7e725df577c
tinabhatnagar27 <154892884+tinabhatnagar27@users.noreply.github.com>
2025-01-17 20:33:30 +0530

    backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bucket-a"   # Replace with your S3 bucket name
-    key            = "terraform/terraform.tfstate"      # Path within the bucket
-    region         = "ap-south-1"               # Replace with your AWS region
+    bucket         = "taru-es-hello-7"   
+    key            = "terraform/terraform.tfstate"     
+    region         = "ap-south-1"               
     encrypt        = true
     
   }
