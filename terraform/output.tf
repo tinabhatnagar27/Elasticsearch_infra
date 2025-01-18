1d956859cc5f58d485ce5f5e1e5e715c6d625aef
tinabhatnagar27 <154892884+tinabhatnagar27@users.noreply.github.com>
2025-01-18 16:52:29 +0530

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,14 +3,14 @@ output "vpc_id" {
 }
 
 output "public-subnet1-id" {
-  value = module.subnet.pub-sub-id
+  value = module.subnet.pub-sub1-id
 }
 
-output "private-subnet1-id" {
+output "private-subnet-1-id" {
   value = module.subnet.pri-sub-1-id
 }
 
-output "private-subnet2-id" {
+output "private-subnet-2-id" {
   value = module.subnet.pri-sub-2-id
 }
 
@@ -24,26 +24,26 @@ output "private-security-groups" {
 
 # Instance ID
 output "elasticsearch-private-1-id" {
-  value = module.instance.elasticsearch-private-1-id
+  value = module.Instance.elasticsearch-private-1-id
 }
 
 output "elasticsearch-private-2-id" {
-  value = module.instance.elasticsearch-private-2-id
+  value = module.Instance.elasticsearch-private-2-id
 }
 
 output "elasticsearch-public-id" {
-  value = module.instance.elasticsearch-public-id
+  value = module.Instance.elasticsearch-public-id
 }
 
 # Instance IP Address
 output "elasticsearch-private-1-ip" {
-  value = module.instance.elasticsearch-private-1-ip
+  value = module.Instance.elasticsearch-private-1-ip
 }
 
 output "elasticsearch-private-2-ip" {
-  value = module.instance.elasticsearch-private-2-ip
+  value = module.Instance.elasticsearch-private-2-ip
 }
 
 output "elasticsearch-public-ip" {
-  value = module.instance.elasticsearch-public-ip
+  value = module.Instance.elasticsearch-public-ip
 }
