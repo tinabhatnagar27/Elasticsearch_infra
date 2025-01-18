d929d06bec30e52121efb9544fcac1b7a887dcc4
tinabhatnagar27 <154892884+tinabhatnagar27@users.noreply.github.com>
2025-01-18 15:51:38 +0530

    Update output.tf

--- terraform/Instances/output.tf
+++ terraform/Instances/output.tf
@@ -1,25 +1,25 @@
 #instenses ID
 output "elasticsearch-public-id" {
-  value = aws_instance.elasticsearch-public.id
+  value = aws_instance.elasticsearch-public-id
 }
 
 output "elasticsearch-private-1-id" {
-  value = aws_instance.elasticsearch-private-1.id
+  value = aws_instance.elasticsearch-private-1-id
 }
 
 output "elasticsearch-private-2-id" {
-  value = aws_instance.elasticsearch-private-2.id
+  value = aws_instance.elasticsearch-private-2-id
 }
 
 # instences IP addresses
 output "elasticsearch-public-ip" {
-  value = aws_instance.elasticsearch-public.public_ip
+  value = aws_instance.elasticsearch-public-ip
 }
 
 output "elasticsearch-private-1-ip" {
-  value = aws_instance.elasticsearch-private-1.private_ip
+  value = aws_instance.elasticsearch-private-1-ip
 }
 
 output "elasticsearch-private-2-ip" {
-  value = aws_instance.elasticsearch-private-2.private_ip
+  value = aws_instance.elasticsearch-private-2-ip
 }
