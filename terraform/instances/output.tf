3c2a865d86336aa4f5381863c4f3b892943d7dc6
tinabhatnagar27 <154892884+tinabhatnagar27@users.noreply.github.com>
2025-01-18 20:03:57 +0530

    Update output.tf

--- terraform/instances/output.tf
+++ terraform/instances/output.tf
@@ -1,1 +1,25 @@
+#instenses ID
+output "pub-instance-id" {
+  value = aws_instance.es-public.id
+}
 
+output "private-instance1-id" {
+  value = aws_instance.es-private-1.id
+}
+
+output "private-instance2-id" {
+  value = aws_instance.es-private-2
+}
+
+# instences IP addresses
+output "public-instance-ip" {
+  value = aws_instance.es-public.public_ip
+}
+
+output "private-instance1-ip" {
+  value = aws_instance.es-private-1.private_ip
+}
+
+output "private-instance2-ip" {
+  value = aws_instance.es-private-2.private_ip
+}
